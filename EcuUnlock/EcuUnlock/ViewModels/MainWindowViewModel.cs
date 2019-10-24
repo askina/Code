85d86ea054d6fc8a1f280db4e0c83742e5dbd8c3
askina <huangjunren969@outlook.com>
2019-10-24 09:53:27 +0800

    测试专用

---
+++ EcuUnlock/EcuUnlock/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+
+namespace EcuUnlock.ViewModels
+{
+    class MainWindowViewModel:NotificationObject
+    {
+        private string op10ProductSerialNmuber;
+
+        public string Op10ProductSerialNumber
+        {
+            get { return op10ProductSerialNmuber; }
+            set {
+                op10ProductSerialNmuber = value;
+                this.RaisePropertyChanged("Op10ProductSerialNumber");
+            }
+        }
+        private Brush operationResultColor = new SolidColorBrush(Colors.LightGreen);
+
+        public Brush OperationResultColor
+        {
+            get { return operationResultColor; }
+            set { operationResultColor = value;
+                RaisePropertyChanged("OperationResultColor");
+               
+            }
+        }
+
+        public void Op10Count()
+        {
+            int i = 0;
+            Action<int> action1 = ((qi) => this.OperationResultColor = new SolidColorBrush(Colors.LightGreen));
+            Action action2 = (() => this.OperationResultColor = new SolidColorBrush(Colors.Red));
+            while (true)
+            {
+                i++;
+                Op10ProductSerialNumber = Convert.ToString(i);
+                if (i%2==0)
+                {
+                    if (this.OperationResultColor.Dispatcher!=null)
+                    {
+                        this.OperationResultColor.Dispatcher.BeginInvoke(action1);
+                    }
+                   
+                }
+                else
+                {
+                    if (this.OperationResultColor.Dispatcher != null)
+                    {
+                        this.OperationResultColor.Dispatcher.BeginInvoke(action2);
+                    }
+                }
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Task.Run(() => { this.Op10Count(); });
+          
+        }
+
+    }
+}
---
+++ EcuUnlock/StationConrol/UserControl1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StationConrol
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        //ProductSerialNmuber_tb
+        public static readonly DependencyProperty ProductSerialNumberProperty = DependencyProperty.Register("ProductSerialNumber", typeof(string), typeof(UserControl1));
+        public string ProductSerialNumber
+        {
+            get { return (string)GetValue(ProductSerialNumberProperty); }
+            set { SetValue(ProductSerialNumberProperty, value); }
+        }
+        //ECUSerialNumber1_tb
+        public static readonly DependencyProperty EcuSerialNumber1Property = DependencyProperty.Register("EcuSerialNumber1", typeof(string), typeof(UserControl1));
+        public string EcuSerialNumber1 {
+            get { return (string)GetValue(EcuSerialNumber1Property); }
+            set { SetValue(EcuSerialNumber1Property, value); } }
+        //EcuSerialNumber2_tb
+        public static readonly DependencyProperty EcuSerialNumber2Property = DependencyProperty.Register("EcuSerialNumber2", typeof(string), typeof(UserControl1));
+        public string EcuSerialNumber2 { get { return (string)GetValue(EcuSerialNumber2Property); }
+            set { SetValue(EcuSerialNumber2Property, value); } }
+        //EcuMessage
+        public static readonly DependencyProperty EcuMessageProperty = DependencyProperty.Register("EcuMessage", typeof(string), typeof(UserControl1));
+        public string EcuMessage {
+            get { return (string)GetValue(EcuMessageProperty); }
+            set { SetValue(EcuMessageProperty, value); } }
+        //OperationResultText
+        public static readonly DependencyProperty OperationResultTextProperty = DependencyProperty.Register("OperationResultText", typeof(string), typeof(UserControl1));
+        public string OperationResultText {
+            get { return (string)GetValue(OperationResultTextProperty); }
+            set { SetValue(OperationResultTextProperty, value); } }
+        //OperationResultColor
+        public static readonly DependencyProperty OperationResultColorProperty = DependencyProperty.Register("OperationResultColor", typeof(Brush), typeof(UserControl1));
+        public Brush OperationResultColor {
+            get { return (Brush)GetValue(OperationResultColorProperty); }
+            set { SetValue(OperationResultColorProperty, value); } }
+    }
+    
+
+}
+
